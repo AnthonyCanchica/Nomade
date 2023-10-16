dfdb519834dd4ff739d4ddeda7ed5e39b46d34de
Guillermo lorenzo <guillermolorenzo89@gmail.com>
2023-10-16 13:30:05 -0300

    cambie el eventos, si consiguen fotos  mejor yo recorte las que habia. agregue logos al footer y mejore la index. arregle tambien la tabla ( que yo rompi)

--- javaScripts/dom.js
+++ javaScripts/dom.js
@@ -4,28 +4,32 @@ footerContainer.innerHTML = `
 <div id="horarios">
     <a href="#top" class="volver-arriba">Volver arriba</a>
 	<h2>HORARIO DE APERTURA</h2>
-	<h3> Miercoles a Sabados 19 HRS</h3>
+	<h3>Miercoles a Sabados 19 HRS</h3>
 </div>
 <div class="contenedor-info">
 	<!-- Direccion -->
 	<div id="direccion">
-		<i id="direccion-logo"></i>
+	<img src="../imagenesDos/logos/alfiler.png" class="logoutm" alt="telefono">
 		<h3>DIRECCIÓN</h3>
-		<p><a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3285.263507090625!2d-58.428881825005966!3d-34.57219845583303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb585ebbf48ed%3A0x73350aa5aaadc57f!2sNomade%20-%20F%26D!5e0!3m2!1ses!2sar!4v1694689456606!5m2!1ses!2sar""><br>
-			Av. Dorrego 3300, C.A.B.A.<br> Argentina</a>
+		<p><a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3285.263507090625!2d-58.428881825005966!3d-34.57219845583303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb585ebbf48ed%3A0x73350aa5aaadc57f!2sNomade%20-%20F%26D!5e0!3m2!1ses!2sar!4v1694689456606!5m2!1ses!2sar" target="_blank">Av. Dorrego 3300<br>
+			Recoleta, Ciudad de Buenos Aires<br> Argentina</a>
         </p>
 	</div>
 	<!-- Numeros -->
 	<div id="telefono">
-		<i id="telefono-logo"></i>
+		<img src="../imagenesDos/logos/informacion.png" class="logoutm" alt="telefono">
 		<h3>TELÉFONO</h3>
-		<p>(+549) 1156074972</p>
+		<p><b>(+549) 1156074972<b></p>
+		<br>
+		<br>
 	</div>
 	<!-- Emails -->
 	<div id="email">
-		<i id="email-logo"></i>
+		<img src="../imagenesDos/logos/correo-electronico.png" class="logoutm" alt="telefono">
 		<h3>Email</h3>
-		<p><a href="mailto:info@parrillaNomade.com.ar" target="_blank">info@parrillaNomade.com.ar</a></p>
+		<p><b><a href="mailto:info@parrillaNomade.com.ar" target="_blank">info@parrillaNomade.com.ar</a><b></p>
+		<br>
+		<br>
 	</div>
 </div>
 	<!-- Social icons -->
