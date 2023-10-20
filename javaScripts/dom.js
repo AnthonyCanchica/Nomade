a53bc650c7968f1789dc3542a8043030a2ed57b3
Guillermo lorenzo <guillermolorenzo89@gmail.com>
2023-10-20 14:44:49 -0300

    cambiados los titulos , hecho api rest y api json en el menu hechop el css , cambiado el quienes (dado formato css) acomodado de eventos y css grid. borrado de carpetas obsoletas

--- javaScripts/dom.js
+++ javaScripts/dom.js
@@ -3,13 +3,14 @@ const footerContainer = document.getElementById("contenedor-footer");
 footerContainer.innerHTML = `
 <div id="horarios">
     <a href="#top" class="volver-arriba">Volver arriba</a>
-	<h2>HORARIO DE APERTURA</h2>
+	<img src="../imagenesDos/logos/tiempo.png" class="logoutm">
+	<h3>HORARIO DE APERTURA</h3>
 	<h3>Miercoles a Sabados 19 HRS</h3>
 </div>
 <div class="contenedor-info">
 	<!-- Direccion -->
 	<div id="direccion">
-	<img src="../imagenesDos/logos/alfiler.png" class="logoutm" alt="telefono">
+	<img src="../imagenesDos/logos/alfiler.png" class="logoutm" >
 		<h3>DIRECCIÓN</h3>
 		<p><a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3285.263507090625!2d-58.428881825005966!3d-34.57219845583303!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb585ebbf48ed%3A0x73350aa5aaadc57f!2sNomade%20-%20F%26D!5e0!3m2!1ses!2sar!4v1694689456606!5m2!1ses!2sar" target="_blank">Av. Dorrego 3300<br>
 			Recoleta, Ciudad de Buenos Aires<br> Argentina</a>
@@ -17,7 +18,7 @@ footerContainer.innerHTML = `
 	</div>
 	<!-- Numeros -->
 	<div id="telefono">
-		<img src="../imagenesDos/logos/informacion.png" class="logoutm" alt="telefono">
+		<img src="../imagenesDos/logos/informacion.png" class="logoutm" >
 		<h3>TELÉFONO</h3>
 		<p><b>(+549) 1156074972<b></p>
 		<br>
@@ -25,7 +26,7 @@ footerContainer.innerHTML = `
 	</div>
 	<!-- Emails -->
 	<div id="email">
-		<img src="../imagenesDos/logos/correo-electronico.png" class="logoutm" alt="telefono">
+		<img src="../imagenesDos/logos/correo-electronico.png" class="logoutm" >
 		<h3>Email</h3>
 		<p><b><a href="mailto:info@parrillaNomade.com.ar" target="_blank">info@parrillaNomade.com.ar</a><b></p>
 		<br>
