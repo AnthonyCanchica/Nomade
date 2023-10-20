5f0ee6fbe50547d65fdea4c19ce501c8dec3df3d
Guillermo lorenzo <guillermolorenzo89@gmail.com>
2023-10-20 19:59:01 -0300

    movi el script de video a su propio js

--- javaScripts/video.js
+++ javaScripts/video.js
@@ -1,4 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const contenidoNosotros = document.querySelector('.contenido-nosotros');
+    const videoElement = document.querySelector('.video-nosotros video');
+    const audioElement = document.querySelector('.video-nosotros audio');
 
+    contenidoNosotros.addEventListener('mouseenter', function () {
+        // Reproducir el video y la música cuando el mouse entra
+        videoElement.play();
+        audioElement.play();
+    });
+
+    contenidoNosotros.addEventListener('mouseleave', function () {
+        // Pausar el video y la música cuando el mouse sale
+        videoElement.pause();
+        audioElement.pause();
+    });
+});
 
 
 
